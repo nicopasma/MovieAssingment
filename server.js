7b44f63fe471c378bfe9e53fd318e27e73f8c780
nicopasma <n3pani00@students.oamk.fi>
2024-12-16 18:46:42 +0200

    Server updated

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ app.get('/', (req, res) => {
     res.send('Hello world');
 });
 
-// Start the server
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
